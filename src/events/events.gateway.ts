61163572d52de61dc30115200932c22058742138
kaluna <kalinamono@gmail.com>
2023-03-10 18:08:55 +0300

    room integration

--- src/events/events.gateway.ts
+++ src/events/events.gateway.ts
@@ -1,11 +1,24 @@
 import { SubscribeMessage, WebSocketGateway } from '@nestjs/websockets';
 import { Socket } from 'socket.io';
+import { v4 as uuidv4 } from 'uuid';
 
 @WebSocketGateway({ cors: true })
 export class EventsGateway {
   @SubscribeMessage('connection')
-  handleMessage(client: Socket, payload: string): void {
-    client.emit('stream', 'blabla');
+  handleConnection(client: Socket, payload: string): void {
+    const roomId = uuidv4();
+    client.on('getRoom', () => {
+      console.log('GetR');
+      client.emit('getRoom', roomId);
+    });
+    client.on('joinRoom', (roomId) => {
+      console.log('joinRoom', roomId);
+      client.join(roomId);
+      client.to(roomId).emit('userConnected', roomId);
+    });
+    client.on('disconnect', () => {
+      console.log('disconnect');
+    });
   }
 }
 
