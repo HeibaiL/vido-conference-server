cd39e2999bcc867d6cb490355c2f822b9ec6db1a
HeibaiL <vladikd96@gmail.com>
2023-05-19 10:51:36 +0300

    updated logic for websockets

--- src/events/events.gateway.ts
+++ src/events/events.gateway.ts
@@ -7,11 +7,18 @@ export class EventsGateway {
 
   @SubscribeMessage('connection')
   handleConnection(client: Socket): void {
-    client.on('joinRoom', (roomId, userId) => {
-      this.allClients.push({ ...client, userId });
+    client.on('joinRoom', (roomId, { userId, name }) => {
+      client.emit(
+        'connectedUsers',
+        this.allClients.map((client) =>
+          JSON.stringify({ userId: client.userId, name: client.name }),
+        ),
+      );
+
+      this.allClients.push({ ...client, userId, name });
 
       client.join(roomId);
-      client.to(roomId).emit('userConnected', userId);
+      client.to(roomId).emit('userConnected', { userId, name });
 
       client.on('leaveRoom', (roomId, userId) => {
         client.to(roomId).emit('userDisconnected', userId);
